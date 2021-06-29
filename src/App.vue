5deedc71946e4a5f0fc001bdf9b56ebb96cd0d2d
wooseok822 <bwoo0822@naver.com>
2021-06-29 18:45:36 +0900

    add deploy

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your wooseok App" />
+  <HelloWorld msg="Welcome to Your wooseok822 App" />
 </template>
 
 <script>
